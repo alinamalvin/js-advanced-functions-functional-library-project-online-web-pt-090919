418ff43fa7ab36ea057b2d3bbe19be945aaff61c
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-22 19:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ const fi = (function() {
       }
       return filteredArr
     },
+    
+     size: function(collection) {
+      return (collection instanceof Array) ? collection.length : Object.keys(collection).length
+    },	    },
 
 
   }
